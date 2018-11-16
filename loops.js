090e8a20df6e4df99e63985951d5b171561e8531
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 04:59:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(arr) {
   for(var i = 0; i < 25; i++) {
-    arr.push("I am " + i + "strange loop.");
+    if(i === 1) {
+      arr.push(`I am ${i} strange loop.`);
+    } else {
+      arr.push(`I am ${i} strange loops.`);
+    }
   }
 }
