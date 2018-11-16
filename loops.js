766fe404a45b202ead492076978ac328fabace36
Raymond Chow <raychow1944@yahoo.com>
2018-11-16 05:02:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
 function forLoop(arr) {
   for(let i = 0; i < 25; i++) {
-    if(i === 1) {
-      arr.push(`I am ${i} strange loop.`);
-    } else {
-      arr.push(`I am ${i} strange loops.`);
-    }
+    i === 1 ? arr.push(`I am ${i} strange loop.`) : arr.push(`I am ${i} strange loops.`);
   }
   return arr;
 }
